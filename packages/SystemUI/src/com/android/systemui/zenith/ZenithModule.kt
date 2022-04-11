94618543c599169a8e59d698bf63bcbebf0eb3f0
cjh1249131356 <cjh1249131356@gmail.com>
2022-04-11 19:41:45 +0800

    SystemUI: Introduce preferred network tile
    **Adapt to Android 14 Matrixx
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>
    Co-Authored-By: Roxor-007 <koyande72@gmail.com>
    Co-Authored-By: ∂α૨ҡ_ρɦσεɳเא <80503611+drkphnx@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/zenith/ZenithModule.kt
+++ packages/SystemUI/src/com/android/systemui/zenith/ZenithModule.kt
@@ -29,7 +29,7 @@ import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VpnTile
 import com.android.systemui.qs.tiles.WifiTile
-
+import com.android.systemui.qs.tiles.PreferredNetworkTile
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -108,4 +108,10 @@ interface ZenithModule {
     @IntoMap
     @StringKey(WifiTile.TILE_SPEC)
     fun bindWifiTile(wifiTile: WifiTile): QSTileImpl<*>
+
+    /** Inject PreferredNetworkTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(PreferredNetworkTile.TILE_SPEC)
+    fun bindPreferredNetworkTile(preferredNetworkTile: PreferredNetworkTile): QSTileImpl<*>
 }
