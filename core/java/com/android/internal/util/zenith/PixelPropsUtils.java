27320754b5fbe7c748d884889959f8e332ae120a
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2025-02-10 14:14:55 +0330

    PixelPropsUtils: fix a typo

--- core/java/com/android/internal/util/zenith/PixelPropsUtils.java
+++ core/java/com/android/internal/util/zenith/PixelPropsUtils.java
@@ -41,7 +41,7 @@ import android.util.Log;
 import android.view.WindowManager;
 
 import com.android.internal.R;
-import com.android.internal.util.zenith.Utils;
+import com.android.internal.util.zenith.utils;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
